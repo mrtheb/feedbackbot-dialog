25a33732631dab5d5484157b86b8d2866ab1d9f4
Eric Sexton <nocrates@gmail.com>
2017-10-17 10:56:44 -0700

    Including requests library for dialog.open call

--- slack_command_handler/slack_command_handler.py
+++ slack_command_handler/slack_command_handler.py
@@ -8,7 +8,9 @@ import os
 import sys
 import traceback
 import urlparse
+import json
 import logging
+import requests
 import boto3
 
 LOGGER = logging.getLogger()
@@ -16,8 +18,22 @@ LOGGER.setLevel(logging.DEBUG)
 
 SNS = boto3.client('sns')
 
+#  These two environment variables are required
+
+# Slack Verification Token is the App Token that Slack provides when you create a Slack App.
+# This uniquely identifier is included in payloads from Slack as proof of authentication
+
 SLACK_VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN')
 
+# Bot Access Token is the token unique to your bot's instance on your customer's slack team.
+# This is obtained when your customers install your bot.
+
+BOT_ACCESS_TOKEN = os.environ.get('BOT_ACCESS_TOKEN')
+
+
+#
+#  A few helper tools to decode x-www-form-urlencoded into dictionary format
+#
 class DecoderError(Exception):
     ''' Exception class for decoder '''
     pass
@@ -42,6 +58,62 @@ def decode_urlencoded(body):
 
     return data
 
+#
+#
+#
+
+def send_slack_dialog(feedback_text=None, bot_access_token=None, trigger_id=None, response_url=None):
+    ''' send slack message to channel '''
+    # Slack API requires both the bot_access_token and a trigger_id.
+    # The trigger_id is provided when a user initiates a command or interactive message (button, or picklist)
+    # Note that trigger_id is time-sensitive, as it expires within seconds of being issued.
+    #  We encode the response_url in the callback_id so that we can send the user an ephemeral Thank you message
+
+    dialog_msg = {
+        "token": bot_access_token,
+        "trigger_id": trigger_id,
+        "dialog" : json.dumps({
+            "title": "Feedback for Woobot",
+            "callback_id": "Feedback__%s" % response_url,
+            "submit_label": "Send",
+            "elements": [{
+                'type': "textarea",
+                'label': 'Comments',
+                'name': 'Comments',
+                'hint': '(required)',
+                'value': feedback_text
+            }]
+        })
+    }
+
+    #  We use the requests library, but other libraries would work as well
+    response = requests.post(
+        url='https://slack.com/api/dialog.open',
+        params=dialog_msg,
+        headers={'Content-type': 'application/json; charset=utf-8'}
+        )
+
+    if response.status_code > 299:  # bad request, or a system problem
+        LOGGER.warning('Slack response code: %d, with text: %s\nAttempted to send: %s', response.status_code, response.text, dialog_msg)
+    else:
+        respobj = json.loads(response.text)
+        # Slack usually responds with a 200 and embeds the actual response in JSON.
+        #
+        # An error message looks like:
+        #    { 'ok': false, 'error': 'Error message' }
+        #
+        # A success looks like:
+        #    { 'ok': true }
+        #
+        if 'ok' in respobj and not respobj['ok']:
+            LOGGER.warning('Error sending to Slack endpoint: %s\nAttempted to send: %s', respobj['error'] if 'error' in respobj else '', dialog_msg)
+
+    return None
+
+#
+#  Handler for lambda invocation from API Gateway
+#
+
 def lambda_handler(event, context):
     ''' Entry point for API Gateway '''
 
@@ -60,6 +132,17 @@ def lambda_handler(event, context):
             LOGGER.warning('Payload received from unverified source')
             raise ValueError('Verification token mismatch')
 
+        if 'trigger_id' not in payload or not payload['trigger_id']:
+            LOGGER.warning('trigger_id not found in payload')
+            raise ValueError('Missing trigger_id')
+
+        send_slack_dialog(
+            feedback_text=payload['text'] if 'text' in payload else None,
+            trigger_id=payload['trigger_id'],
+            bot_access_token=BOT_ACCESS_TOKEN,
+            response_url=payload['response_url'] if 'response_url' in payload else None
+            )
+
     except Exception: # pylint: disable=W0703
         # Do not ever let them see you sweat. Hide internal error messages from the user.
         LOGGER.warning("ERROR: Unexpected error:\n".join(traceback.format_exception(*(sys.exc_info()))))
@@ -68,6 +151,8 @@ def lambda_handler(event, context):
     finally:
         #  The user will receive a dialog open event through the dialog.open request call.
         #  This return result is for the slack command. Success is a 200 with an empty body.
+
+        #  Note that this response object is specific to AWS API Gateway
         return { # pylint: disable=W0150
             'statusCode': 200,
             'headers': {'Content-type': 'application/json'},
